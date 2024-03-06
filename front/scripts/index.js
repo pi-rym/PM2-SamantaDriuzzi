3ecf94ebd0401b403e9bcdc42dca3c8b2ced4b61
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2024-03-06 17:25:33 -0300

    ajustes de estilos en casi toda la aplicacion.Pagina 'inicio' finalizada

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,21 +1,29 @@
 const movieCardsContainer = document.getElementById("movieCardsContainer");
 
-const movies = tempData;
-
-movies.forEach((movie) => {
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  const img = document.createElement("img");
-  img.src = movie.poster;
-  img.alt = `${movie.title} Poster`;
-
-  const nameParagraph = document.createElement("p");
-  nameParagraph.textContent = movie.title;
-  nameParagraph.classList.add("name");
-
-  card.appendChild(img);
-  card.appendChild(nameParagraph);
-
-  movieCardsContainer.appendChild(card);
+$.get("https://students-api.2.us-1.fl0.io/movies", (data) => {
+  cardMovie(data);
 });
+
+const cardMovie = (data) => {
+  data.forEach((movie, index) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const numberContainer = document.createElement("span");
+    numberContainer.classList.add("number");
+    numberContainer.textContent = index + 1;
+    card.appendChild(numberContainer);
+
+    const img = document.createElement("img");
+    img.src = movie.poster;
+    img.alt = `${movie.title} Poster`;
+    card.appendChild(img);
+
+    const nameParagraph = document.createElement("p");
+    nameParagraph.textContent = movie.title;
+    nameParagraph.classList.add("name");
+    card.appendChild(nameParagraph);
+
+    movieCardsContainer.appendChild(card);
+  });
+};
