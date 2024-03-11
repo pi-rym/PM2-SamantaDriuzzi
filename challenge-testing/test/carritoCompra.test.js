88eba77edc7b77b0a94ff07e6f3f10552d24a2fe
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2024-03-11 18:52:59 -0300

    Se hacen mejoras en el código de test

--- challenge-testing/test/carritoCompra.test.js
+++ challenge-testing/test/carritoCompra.test.js
@@ -1,54 +1,75 @@
-// Desarrolla una clase en JavaScript llamada CarritoCompra que represente un carrito de compras.
-// La clase debe tener los siguientes métodos:
-//
-// constructor(): Inicializa el carrito como un array vacío.
-//
-// agregarProducto(producto): Recibe un objeto representando un producto y lo agrega al carrito.
-//
-// calcularTotal(): Calcula el total de la compra sumando los precios de todos los productos en el carrito.
-//
-// aplicarDescuento(porcentaje): Aplica un descuento al total de la compra según el porcentaje especificado.
-
 const carritoCompra = require("./index");
 
 describe("Test para la clase CarritoCompra", () => {
+  product1 = {
+    name: "Mesa",
+    price: 100,
+    quantity: 1,
+  };
+  product2 = {
+    name: "Silla",
+    price: 200,
+    quantity: 1,
+  };
+
   it("CarritoCompra debe ser una clase", () => {
-    expect(carritoCompra).toBeInstanceOf(Object);
+    // expect(carritoCompra).toBeInstanceOf(Object);
+    expect(typeof carritoCompra.prototype.constructor).toBe("function");
+  });
+
+  it("carrito debe ser una instancia de CarritoCompra", () => {
+    const carrito = new carritoCompra();
+    expect(carrito).toBeInstanceOf(carritoCompra);
   });
 
   it("Debe tener el método constructor() que inicializa el carrito como un array vacío", () => {
-    const carrito = new carritoCompra();
-    expect(carrito.carrito).toEqual([]);
+    const { carrito } = new carritoCompra();
+    expect(carrito).toEqual([]);
+    expect(carrito).not.toEqual([{ product1 }]);
   });
 
   it("Debe tener el método agregarProducto(producto)", () => {
     const carrito = new carritoCompra();
-    expect(carrito.agregarProducto).toBeDefined();
+    expect(typeof carrito.agregarProducto).toBe("function");
   });
 
   it("Debe tener el método calcualarTotal()", () => {
     const carrito = new carritoCompra();
-    expect(carrito.calcularTotal).toBeDefined();
+    expect(typeof carrito.calcularTotal).toBe("function");
   });
+
   it("Debe tener el método aplicarDescuento(porcentaje)", () => {
     const carrito = new carritoCompra();
-    expect(carrito.aplicarDescuento).toBeDefined();
+    expect(typeof carrito.aplicarDescuento).toBe("function");
   });
+
   it("El método agregarProducto(producto) debe agregar un producto a carrito", () => {
     const carrito = new carritoCompra();
-    carrito.agregarProducto({ name: "Mesa", price: 100 });
-    expect(carrito.carrito).toEqual([{ name: "Mesa", price: 100 }]);
+
+    // Se agrega producto
+    carrito.agregarProducto(product1);
+
+    expect(carrito.carrito).toEqual([product1]);
   });
+
   it("El método calcularTotal() debe retornar la suma total de los productos en el carrito", () => {
     const carrito = new carritoCompra();
-    carrito.agregarProducto({ name: "Mesa", price: 100 });
-    carrito.agregarProducto({ name: "Silla", price: 200 });
-    expect(carrito.calcularTotal()).toBe(300);
+    carrito.agregarProducto(product1);
+    const totalEsperado = product1.price * product1.quantity;
+    expect(carrito.calcularTotal()).toEqual(totalEsperado);
   });
+
   it("El método aplicarDescuento(porcentaje) debe aplicar un descuento al total de la compra", () => {
     const carrito = new carritoCompra();
-    carrito.agregarProducto({ name: "Mesa", price: 100 });
-    carrito.agregarProducto({ name: "Silla", price: 200 });
-    expect(carrito.aplicarDescuento(10)).toBe(270);
+    carrito.agregarProducto(product1);
+    carrito.agregarProducto(product2);
+
+    const subtotal =
+      product1.price * product1.quantity + product2.price * product2.quantity;
+    const porcentaje = 10;
+    const totalConDescuentoEsperado = subtotal - subtotal * (porcentaje / 100);
+    const total = carrito.aplicarDescuento(porcentaje);
+
+    expect(total).toEqual(totalConDescuentoEsperado);
   });
 });
--- challenge-testing/test/index.js
+++ challenge-testing/test/index.js
@@ -1,18 +1,3 @@
-// // Desarrolla una clase en JavaScript llamada CarritoCompra que represente
-//    un carrito de compras.
-// // La clase debe tener los siguientes métodos:
-// //
-// // constructor(): Inicializa el carrito como un array vacío.
-// //
-// // agregarProducto(producto): Recibe un objeto representando un producto y
-//    lo agrega al carrito.
-// //
-// // calcularTotal(): Calcula el total de la compra sumando los precios de todos
-//    los productos en el carrito.
-// //
-// // aplicarDescuento(porcentaje): Aplica un descuento al total de la compra según
-//    el porcentaje especificado.
-
 class carritoCompra {
   constructor() {
     this.carrito = [];
@@ -23,7 +8,7 @@ class carritoCompra {
   calcularTotal() {
     let total = 0;
     for (const item of this.carrito) {
-      total += item.price;
+      total += item.price * item.quantity;
     }
     return total;
   }
