f01dd5934065509de89425d171d95c0ca23dfa2f
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2024-03-12 11:42:31 -0300

    Ajustes en la url de la api y ajustes en el manejo del error con axios

--- front/scripts/handler.js
+++ front/scripts/handler.js
@@ -1,8 +1,37 @@
 const cardMovie = require("./cardMovie");
 
-const getMovie = () => {
-  $.get("https://students-api.2.us-1.fl0.io/movies", (data) => {
-    cardMovie(data);
-  });
+const axios = require("axios");
+
+// const promise = axios.get(" https://students-api.up.railway.app/movies");
+//
+// promise
+//   .then((response) => {
+//     cardMovie(response.data);
+//   })
+//   .cath((err) => {
+//     console.error(err);
+//     alert("Error: " + err.message);
+//   });
+
+const getMovie = async () => {
+  try {
+    const response = await axios.get(
+      "https://students-api.up.railway.app/movi"
+    );
+    cardMovie(response.data);
+  } catch (err) {
+    //si el error es 404
+    if (err.response.status === 404) {
+      let fondo = "../../assets/fondoError.png";
+      document.body.style.backgroundImage = `url('${fondo}')`;
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundRepeat = "no-repeat";
+    }
+    //si no mostrar alerta con el error
+    else {
+      alert("Error: " + err.message);
+    }
+  }
 };
+
 module.exports = getMovie;
